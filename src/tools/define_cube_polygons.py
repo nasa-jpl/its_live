78534ebeec81ef2a2e09a5421bc07a645dcd08af
Masha Liukis <mliukis@jpl.nasa.gov>
2021-05-07 16:15:22 -0700

    Store all cubes in one geojson file; Use geometry as lon/lat coordinates for all cubes; Keep "empty" cubes for EPSG with ROI coverage

--- src/tools/define_cube_polygons.py
+++ src/tools/define_cube_polygons.py
@@ -3,6 +3,7 @@ Script to define polygons for the datacube production.
 """
 import geopandas as gpd
 import json
+import geojson
 import logging
 import numpy as np
 import os
@@ -29,22 +30,24 @@ logging.basicConfig(
 
 logger = logging.getLogger("define_datacube")
 
-def define_cubes(shapefile: str, grid_size: int = 100000, cube_dir: str = 'datacube_definitions'):
+def define_cubes(shape_filename: str, cube_filename: str, grid_size: int = 100000):
     """
     Function to define ITS_LIVE data cubes based on provided shape file and grid
     size in meters.
     """
-    # File is in s3 bucket
-    # if shapefile.startswith('s3'):
+    # TODO: Read the file from s3 bucket
+    # if shape_filename.startswith('s3'):
     #     s3 = s3fs.S3FileSystem(anon=True)
 
-    shapefile = gpd.read_file('/Users/mliukis/Documents/ITS_LIVE/fromAlex/autorift_landice_0120m/autorift_landice_0120m.shp')
+    shapefile = gpd.read_file(shape_filename)
     # print("Shape: ", shapefile.columns)
 
+    features = []
 
     for index, each_row in shapefile.iterrows():
         # Process each polygon from the shapefile
-        epsg_code = each_row.epsg
+        epsg_code = str(each_row.epsg)
+
         logger.info(f"Processing {epsg_code}")
 
         roi_file = each_row.ROI
@@ -70,9 +73,7 @@ def define_cubes(shapefile: str, grid_size: int = 100000, cube_dir: str = 'datac
 
         for each_lon, each_lat in envelope.exterior.coords:
             # print(f"Each lon/lat: {each_lon} {each_lat}")
-            epsg_polygon.append(itslive_utils.transform_coord(LON_LAT_PROJECTION, str(epsg_code), each_lon, each_lat))
-
-        # print(f"epsg_code={epsg_code}: {os.linesep.join(str(each) for each in epsg_polygon)}")
+            epsg_polygon.append(itslive_utils.transform_coord(LON_LAT_PROJECTION, epsg_code, each_lon, each_lat))
 
         # Find bounding box for the polygon in EPSG regional code
         x_bounds = Bounds([each[0] for each in epsg_polygon])
@@ -82,17 +83,13 @@ def define_cubes(shapefile: str, grid_size: int = 100000, cube_dir: str = 'datac
         new_x = x_bounds.extend_to_grid(grid_size)
         new_y = y_bounds.extend_to_grid(grid_size)
 
-        # print("After extends:")
-        # str(new_x)
-        # str(new_y)
-
         # Define grid for the data cubes
         x_range = list(range(new_x.min,  new_x.max, grid_size))
         y_range = list(range(new_y.min,  new_y.max, grid_size))
 
         # Capture longitude and latitude for original polygon
         lon, lat = envelope.exterior.coords.xy
-        cube_definition = {'epsg': int(epsg_code),
+        cube_definition = {'epsg': epsg_code,
                            'cubes_x_y': [],
                            'epsg_polygon': epsg_polygon,
                            'cube_size_m': grid_size,
@@ -107,6 +104,10 @@ def define_cubes(shapefile: str, grid_size: int = 100000, cube_dir: str = 'datac
         print(f"ROI data.shape: {roi_data.shape}")
         print(f"ROI.GetGeoTransform: {roi_ds.GetGeoTransform()}")
         print(f"ROI.XSize: {roi_ds.GetRasterBand(1).XSize} ROI.YSize: {roi_ds.GetRasterBand(1).YSize}")
+
+        # Get the cube polygon size in pixels
+        cube_num_cells = int(grid_size/roi_ds.GetGeoTransform()[1])**2
+        logger.info(f"Cube size: {cube_num_cells}")
 
         roi_x = np.array(np.arange(roi_geo_trans[0], roi_geo_trans[0] + roi_geo_trans[1]*roi_xsize, roi_geo_trans[1]))
         roi_y = np.array(np.arange(roi_geo_trans[3], roi_geo_trans[3] + roi_geo_trans[5]*roi_ysize, roi_geo_trans[5]))
@@ -134,24 +135,60 @@ def define_cubes(shapefile: str, grid_size: int = 100000, cube_dir: str = 'datac
                 min_y_ind, max_y_ind = sel[0][0], sel[0][-1]+1
                 # print(f"Selection y: num={len(sel[0])}: min={min_y_ind} max={max_y_ind}")
 
+                # Convert cube polygon to lon/lat coordinates
+                lonlat_coords = [itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_min, cube_y_min)]
+                lonlat_coords.append(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_max, cube_y_min))
+                lonlat_coords.append(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_max, cube_y_max))
+                lonlat_coords.append(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_min, cube_y_max))
+                lonlat_coords.append(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_min, cube_y_min))
+
                 # TODO: Apply ROI for each of the cubes to see if it contains any ROI pixels
-                if roi_data[min_y_ind:max_y_ind, min_x_ind:max_x_ind].sum() == 0:
-                    logger.info(f"Skipping cube x=[{cube_x_min}, {cube_x_max}] y=[{cube_y_min}, {cube_y_max}] due to no ROI data")
+                # if roi_data[min_y_ind:max_y_ind, min_x_ind:max_x_ind].sum() == 0:
+                #     logger.info(f"Skipping cube x=[{cube_x_min}, {cube_x_max}] y=[{cube_y_min}, {cube_y_max}] due to no ROI data")
+                #
+                # else:
+                # TODO: Should "revert" y values: max, min
+                # cube_definition['cubes_x_y'].append([(cube_x_min, cube_x_max), (cube_y_min, cube_y_max)])
 
-                else:
-                    # TODO: Should "revert" y values: max, min
-                    cube_definition['cubes_x_y'].append([(cube_x_min, cube_x_max), (cube_y_min, cube_y_max)])
+                roi_coverage = roi_data[min_y_ind:max_y_ind, min_x_ind:max_x_ind].sum()/cube_num_cells
 
-        cube_definition['num_cubes'] = len(cube_definition['cubes_x_y'])
+                # Capture lon/lat coordinates for the cube
+                features.append(
+                    geojson.Feature(
+                        geometry=geojson.Polygon([lonlat_coords]),
+                        properties={
+                            # "stroke-width": 2,
+                            # "stroke-opacity": 1,
+                            "fill-opacity": 1.0 - roi_coverage,
+                            "fill": "red",
+                            'roi_percent_coverage': roi_coverage*100,
+                            'data_epsg':    epsg_code,
+                            'geometry_epsg': geojson.Polygon([[
+                                [cube_x_min, cube_y_min],
+                                [cube_x_max, cube_y_min],
+                                [cube_x_max, cube_y_max],
+                                [cube_x_min, cube_y_max],
+                                [cube_x_min, cube_y_min]]]),
+                        }
+                        # style = {
+                        #     "opacity": 1.0 - roi_coverage,
+                        #     # "fill-opacity": 1.0 - roi_coverage,
+                        #     # "opacity": 1.0 - roi_coverage,
+                        #     "fill": "green" if roi_coverage > 0.5 else "yellow",
+                        #     # "fill": "green" if roi_coverage > 0.5 else "yellow",
+                        #
+                        # }
+                    )
+                )
 
-        if cube_definition['num_cubes'] == 0:
-            # There were no cubes with ROI
-            logger.info(f"No cubes defined for {epsg_code} due to no data in ROI {roi_file}")
+    feature_collection = geojson.FeatureCollection(features)
 
-        # Write cube definitions to the file
-        with open(f'{cube_dir}/cubeGrid_{epsg_code}.json', 'w+') as fhandle:
-            json.dump(cube_definition, fhandle, indent=4, sort_keys=True)
+    # Write cube definitions to the file
+    with open(cube_filename, 'w+') as fhandle:
+        # json.dump(feature_collection, fhandle, indent=4, sort_keys=True)
+        json.dump(feature_collection, fhandle, indent=4)
 
+    features = None
 
 if __name__ == '__main__':
     import argparse
@@ -163,6 +200,8 @@ if __name__ == '__main__':
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-s', '--shapeFile', type=str, default='/Users/mliukis/Documents/ITS_LIVE/fromAlex/autorift_landice_0120m/autorift_landice_0120m.shp',
                         help='Regional shape file that defines each of the EPSG polygons.')
+    parser.add_argument('-o', '--outputFile', type=str, default='cubeGrid.json',
+                        help='Geojson file to store cube polygon definitions.')
     args = parser.parse_args()
 
-    define_cubes(args.shapeFile)
+    define_cubes(args.shapeFile, args.outputFile)
