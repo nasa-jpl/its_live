ee13ab89d2a4c4e915e2d7a98f7df59ec65ab9b5
Masha Liukis <mliukis@jpl.nasa.gov>
2021-05-14 16:57:06 -0700

    Runtime optimization: replace pyproj.transform with pyproj.Transform.transform (45mins vs. 5 secs per EPGS code); Keep only data cubes that overlap with original lon/lat polygon as defined in shape file

--- src/tools/define_cube_polygons.py
+++ src/tools/define_cube_polygons.py
@@ -13,6 +13,7 @@ import math
 import numpy as np
 import os
 from osgeo import gdal
+from pyproj import Transformer
 from shapely import affinity
 from shapely.geometry import Polygon, LineString, GeometryCollection, mapping, MultiPolygon
 from shapely.ops import split
@@ -28,7 +29,7 @@ gdal.SetConfigOption('CPL_VSIL_CURL_ALLOWED_EXTENSIONS', 'tif')
 gdal.SetConfigOption('VSI_CACHE', 'TRUE')
 gdal.SetConfigOption('GDAL_DISABLE_READDIR_ON_OPEN', 'TRUE')
 
-LON_LAT_PROJECTION = '4326'
+LON_LAT_PROJECTION = 'EPSG:4326'
 
 # Set up logging
 logging.basicConfig(
@@ -56,7 +57,7 @@ def translate_polygons(geometry_collection: GeometryCollection) -> list:
 
     return polygons
 
-def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str = None, grid_size: int = 100000):
+def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: list = None, grid_size: int = 100000):
     """
     Function to define ITS_LIVE data cubes based on provided shape file and grid
     size in meters.
@@ -72,11 +73,13 @@ def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str
 
     for index, each_row in shapefile.iterrows():
         # Process each polygon from the shapefile
-        epsg_code = str(each_row.epsg)
+        epsg_code = int(each_row.epsg)
 
         if target_epsg_codes is not None and epsg_code not in target_epsg_codes:
             continue
 
+        # Format the code for the pyproj.Transformer
+        epsg_code = f"EPSG:{epsg_code}"
         logging.info(f"Processing {epsg_code}")
 
         # Read ROI data in
@@ -93,26 +96,25 @@ def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str
         roi_data = roi_ds.GetRasterBand(1).ReadAsArray()
 
         if roi_data.sum() == 0:
-            logging.info(f"Skipping {epsg_code} due to no data in ROI {roi_file}")
-            continue
+            logging.info(f"{epsg_code}'s ROI {roi_file} has no data")
 
         # GeoTransform: (-32647.5, 120.0, 0.0, 10199047.5, 0.0, -120.0)
         roi_geo_trans = roi_ds.GetGeoTransform()
         roi_xsize = roi_ds.GetRasterBand(1).XSize
         roi_ysize = roi_ds.GetRasterBand(1).YSize
 
-        print(f"ROI data.shape: {roi_data.shape}")
-        print(f"ROI.GetGeoTransform: {roi_ds.GetGeoTransform()}")
-        print(f"ROI.XSize: {roi_ds.GetRasterBand(1).XSize} ROI.YSize: {roi_ds.GetRasterBand(1).YSize}")
+        # logging.info(f"ROI data.shape: {roi_data.shape}")
+        logging.info(f"ROI.GetGeoTransform: {roi_ds.GetGeoTransform()}")
+        logging.info(f"ROI.XSize: {roi_ds.GetRasterBand(1).XSize} ROI.YSize: {roi_ds.GetRasterBand(1).YSize}")
 
         # Get the cube polygon size in pixels
         cube_num_cells = int(grid_size/roi_ds.GetGeoTransform()[1])**2
-        logging.info(f"Cube size: {cube_num_cells}")
+        logging.info(f"Number of cube cells: {cube_num_cells}")
 
         roi_x = np.array(np.arange(roi_geo_trans[0], roi_geo_trans[0] + roi_geo_trans[1]*roi_xsize, roi_geo_trans[1]))
         roi_y = np.array(np.arange(roi_geo_trans[3], roi_geo_trans[3] + roi_geo_trans[5]*roi_ysize, roi_geo_trans[5]))
-        print(f"ROI x: {roi_x.min()} {roi_x.max()}")
-        print(f"ROI y: {roi_y.min()} {roi_y.max()}")
+        # logging.info(f"ROI x: {roi_x.min()} {roi_x.max()}")
+        # logging.info(f"ROI y: {roi_y.min()} {roi_y.max()}")
 
         assert len(roi_x) == roi_xsize, f"Inconsistent x dimension size: {len(roi_x)} vs. {roi_xsize}"
         assert len(roi_y) == roi_ysize, f"Inconsistent y dimension size: {len(roi_y)} vs. {roi_ysize}"
@@ -134,34 +136,35 @@ def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str
             meridian = [[split_meridian, -90.0], [split_meridian, 90.0]]
             splitter = LineString(meridian)
             split_polygons = split(Polygon(envelope), splitter)
-            logging.info(f"Split polygons: {split_polygons}")
+            logging.info(f"Number of split polygons: {len(split_polygons)}")
 
         # Process each polygon (if it was split, otherwise there is only one
         # polygon)
+        transformer = Transformer.from_crs(LON_LAT_PROJECTION, epsg_code, always_xy=True)
+        back_transformer = Transformer.from_crs(epsg_code, LON_LAT_PROJECTION, always_xy=True)
+
         for each_polygon in split_polygons:
             # Convert polygon to target EPSG coords
             epsg_polygon = []
-            lonlat_coords = []
-            for each_lon, each_lat in each_polygon.exterior.coords:
-                if each_lat == 90:
-                    each_lat = 89.999999
-                if each_lat == -90:
-                    each_lat = -89.999999
+            lon, lat = map(np.array, each_polygon.exterior.coords.xy)
+            logging.info(f"Polygon lon: {lon.shape} lat: {lat.shape}")
+            # This is to prevent corner cases when
+            # all original coordinates at latitude=+-90 or longitude=+-180 get
+            # projected to the same x/y=(0;0) in target EPSG
+            lon[np.isclose(lon, -180.0)] = -179.999999
+            lon[np.isclose(lon, 180.0)] = 179.999999
+            lat[np.isclose(lat, -90.0)] = -89.999999
+            lat[np.isclose(lat, 90.0)] = 89.999999
 
-                if each_lon == -180:
-                    each_lon = -179.999999
-                if each_lon == 180:
-                    each_lon = 179.999999
+            # Convert coordinates
+            # epsg_polygon = [pyproj.transform(proj1, proj2, e_lon, e_lat) for e_lon, e_lat in zip(lon, lat)]
+            epsg_polygon = [transformer.transform(e_lon, e_lat) for e_lon, e_lat in zip(lon, lat)]
+            logging.info(f"EPGS region: {len(epsg_polygon)}")
 
-                lonlat_coords.append([each_lon, each_lat])
-                epsg_polygon.append(itslive_utils.transform_coord(LON_LAT_PROJECTION, epsg_code, each_lon, each_lat))
-                # print(f"Converted lon/lat: {each_lon}; {each_lat} to x/y: {epsg_polygon[-1]}")
-
-            # logging.info(f"EPGS region lon/lat: {lonlat_coords}")
-            # # Convert back to debug coverage
+            # # Convert back to lon/lat to debug coverage
             # debug_lonlat = []
             # for each_x, each_y in epsg_polygon:
-            #     debug_lonlat.append(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, each_x, each_y))
+            #     debug_lonlat.append(back_transformer.transform(each_x, each_y))
             #     print(f"Converted x/y: {each_x}; {each_y} to lon/lat: {debug_lonlat[-1]}")
             #
             # logging.info(f"EPGS region lon/lat debug: {debug_lonlat}")
@@ -184,7 +187,7 @@ def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str
 
             # Create x/y ranges for each of the datacubes
             for each_x in tqdm(range(new_x.min, new_x.max, grid_size), ascii=True, desc="Processing X axis..."):
-                for each_y in range(new_y.min, new_y.max, grid_size):
+                for each_y in tqdm(range(new_y.min, new_y.max, grid_size), ascii=True, desc="Processing Y axis..."):
                     cube_x_min, cube_x_max = each_x, each_x + grid_size
                     cube_y_min, cube_y_max = each_y, each_y + grid_size
 
@@ -193,19 +196,20 @@ def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str
                     # print(f"Selection x: {len(sel[0])}")
                     # Inclusive indices of ROI X (pass one for the last index)
                     min_x_ind, max_x_ind = sel[0][0], sel[0][-1]+1
-                    # print(f"Selection x: num={len(sel[0])}: min={min_x_ind} max={max_x_ind}")
 
                     sel = np.where((roi_y >= cube_y_min) & (roi_y < cube_y_max))
                     # Inclusive indices of ROI Y (pass one for the last index)
                     min_y_ind, max_y_ind = sel[0][0], sel[0][-1]+1
-                    # print(f"Selection y: num={len(sel[0])}: min={min_y_ind} max={max_y_ind}")
 
                     # Convert cube polygon to lon/lat coordinates
-                    lonlat_coords = [list(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_min, cube_y_min))]
-                    lonlat_coords.append(list(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_max, cube_y_min)))
-                    lonlat_coords.append(list(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_max, cube_y_max)))
-                    lonlat_coords.append(list(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_min, cube_y_max)))
-                    lonlat_coords.append(list(itslive_utils.transform_coord(epsg_code, LON_LAT_PROJECTION, cube_x_min, cube_y_min)))
+                    lonlat_coords = []
+                    for each_x, each_y in [
+                        (cube_x_min, cube_y_min),
+                        (cube_x_max, cube_y_min),
+                        (cube_x_max, cube_y_max),
+                        (cube_x_min, cube_y_max),
+                        (cube_x_min, cube_y_min)]:
+                        lonlat_coords.append(list(back_transformer.transform(each_x, each_y)))
 
                     # Initialize min/max of the split line
                     minx = maxx = lonlat_coords[0][0]
@@ -230,7 +234,7 @@ def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str
                         if x_shift < minx: minx = x_shift
                         if x_shift > maxx: maxx = x_shift
 
-                    geometry_obj = geojson.Polygon([lonlat_coords])
+                    geometry_obj = Polygon(lonlat_coords)
                     if crosses_antimeridian:
                         # Define meridian to split on
                         split_meridian = -180 if minx < -180 else 180
@@ -239,14 +243,33 @@ def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str
                         splitter = LineString(meridian)
                         split_polygons = split(Polygon(lonlat_coords), splitter)
 
-                        geo_polygons = list(translate_polygons(split_polygons))
-                        geometry_obj = MultiPolygon(geo_polygons)
+                        geo_polygons = translate_polygons(split_polygons)
 
-                    # Skip cube if it does not contain any ROI data
-                    # if roi_data[min_y_ind:max_y_ind, min_x_ind:max_x_ind].sum() == 0:
-                    #     logging.info(f"Skipping cube x=[{cube_x_min}, {cube_x_max}] y=[{cube_y_min}, {cube_y_max}] due to no ROI data")
-                    #     continue
+                        # Check if cube overlaps original polygon in lon/lat coordinates
+                        keep_polygons = []
+                        for each in geo_polygons:
+                            if Polygon(each).intersects(each_polygon):
+                                keep_polygons.append(each)
 
+                        if len(keep_polygons) == 0:
+                            # None of the cube polygons intersect original polygon
+                            geometry_obj = None
+
+                        elif len(keep_polygons) == 1:
+                            geometry_obj = Polygon(keep_polygons[0])
+
+                        else:
+                            geometry_obj = MultiPolygon(keep_polygons)
+
+                    else:
+                        if not geometry_obj.intersects(each_polygon):
+                            geometry_obj = None
+
+                    if geometry_obj is None:
+                        # There is no valid cube to record
+                        continue
+
+                    # Region Of Interest coverage within the cube
                     roi_coverage = roi_data[min_y_ind:max_y_ind, min_x_ind:max_x_ind].sum()/cube_num_cells
 
                     # For each cube capture lon/lat coordinates as geometry
@@ -284,7 +307,6 @@ def define_cubes(shape_filename: str, cube_filename: str, target_epsg_codes: str
 
     # Write cube definitions to the file
     with open(cube_filename, 'w+') as fhandle:
-        # json.dump(feature_collection, fhandle, indent=4, sort_keys=True)
         json.dump(feature_collection, fhandle, indent=4)
 
     features = None
@@ -302,12 +324,12 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--outputFile', type=str, default='cubeGrid.json',
                         help='Geojson file to store cube polygon definitions.')
     parser.add_argument('-c', '--epsgCode', type=str, default=None,
-                        help='EPGS code(s) as json list string to create datacube grid for. Default is None meaning to generate complete datacube grid.')
+                        help='EPGS code(s) as json list to create datacube grid for. Default is None meaning to generate complete datacube grid.')
 
     args = parser.parse_args()
 
     # Map provided EPSG codes to the list of strings
-    epsg_codes = list(map(str, json.loads(args.epsgCode))) if args.epsgCode is not None else None
+    epsg_codes = list(map(int, json.loads(args.epsgCode))) if args.epsgCode is not None else None
     logging.info(f"Got EPGS codes: {epsg_codes}")
 
     define_cubes(args.shapeFile, args.outputFile, epsg_codes)
